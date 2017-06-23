886ad754ff7038ac48952db69850fcad56d32b58
tbscanlon <thomas.scanlon@outlook.com>
2017-06-23 15:43:15 +0100

    Refactor to remove global variable

--- app.rb
+++ app.rb
@@ -5,23 +5,27 @@ require './lib/game'
 class Battle < Sinatra::Base
   enable :sessions
 
+  before do
+    @game = Game.instance
+  end
+
   get '/' do
     erb(:index)
   end
 
   post '/names' do
-    $game = Game.new(Player.new(params[:player_1]), Player.new(params[:player_2]))
+    @game = Game.create(Player.new(params[:player_1]), Player.new(params[:player_2]))
     redirect to('/play')
   end
 
   get '/play' do
-    $game.change_players
-    redirect to('/endgame') if $game.player1.dead? || $game.player2.dead?
+    @game.change_players
+    redirect to('/endgame') if @game.player1.dead? || @game.player2.dead?
     erb(:play)
   end
 
   get '/attack' do
-    $game.attack
+    @game.attack
     erb(:attack)
   end
 
